9b14f2bc098b461573cf24ddf9da3994039c2083
Brandon Mireles <brandon.mireles@hcl.com>
2022-11-08 14:59:03 -0600

    Initial FrontEnd Upload

---
+++ midtermdemo-app/src/app/Services/products.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Products} from '../Models/products.model';
+
+const baseUrl = 'http://localhost:8181/api/Product/products';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductsService {
+    constructor(private http: HttpClient) { }
+
+    getAll(): Observable<Products[]> {
+        return this.http.get<Products[]>(baseUrl);
+    }
+
+    get(id: any): Observable<Products> {
+        return this.http.get<Products>(`${baseUrl}/${id}`);
+    }
+
+    create(data: any): Observable<any> {
+        return this.http.post(baseUrl,data);
+    }
+
+    update(id: any, data: any): Observable<any> {
+        return this.http.put(`${baseUrl}/${id}`,data);
+    }
+
+    delete(id: any): Observable<any> {
+        return this.http.delete(`${baseUrl}/${id}`);
+    }
+
+    deleteAll(): Observable<any> {
+        return this.http.delete(baseUrl);
+    }
+
+    findByName(name: any): Observable<Products[]> {
+        return this.http.get<Products[]>(`${baseUrl}?name=${name}`);
+      }
+}
